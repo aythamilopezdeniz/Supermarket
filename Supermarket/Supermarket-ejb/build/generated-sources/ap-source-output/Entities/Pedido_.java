3d310d30f21e9ce5b1bb25353cb0cb6e0cb12ea4
Aythami López Déniz <aythamilopezdeniz@hotmail.com>
2018-05-15 11:42:05 +0100

    Final

--- Supermarket/Supermarket-ejb/build/generated-sources/ap-source-output/Entities/Pedido_.java
+++ Supermarket/Supermarket-ejb/build/generated-sources/ap-source-output/Entities/Pedido_.java
@@ -8,7 +8,7 @@ import javax.persistence.metamodel.CollectionAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-05-15T09:39:40")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-05-15T11:36:38")
 @StaticMetamodel(Pedido.class)
 public class Pedido_ { 
 
